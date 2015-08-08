562e3c481376dbf606a36e2acbf5256213744595
Hannah <totallyarogue@gmail.com>
2015-08-08 16:55:10 -0700

    corrected static path

--- mysite/settings.py
+++ mysite/settings.py
@@ -96,12 +96,14 @@ USE_L10N = True
 
 USE_TZ = True
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = ''
+# STATIC_ROOT = ''
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
-STATICFILES_DIRS = ( os.path.join('static'), )
+# STATICFILES_DIRS = ( os.path.join('static'), )
